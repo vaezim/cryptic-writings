d76ba97edec5049d0ab4776c9633a33c2745defe
Max Remzi <91099081+vaezim@users.noreply.github.com>
2026-03-21 01:07:50 -0400

    Added CMake option to build server executable only

--- build.sh
+++ build.sh
@@ -6,4 +6,9 @@ if [[ $1 == "clean" ]]; then
     exit 0
 fi
 
+if [[ $1 == "server" ]]; then
+    cmake -S . -B build/ -DSERVER_ONLY=ON && make --no-print-directory -j4 -C build/
+    exit 0
+fi
+
 cmake -S . -B build/ && make --no-print-directory -j4 -C build/
